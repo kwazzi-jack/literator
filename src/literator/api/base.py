8862241128bf17065333bef1ccb2153b7a732dcb
brianwelman2 <brianallisterwelman@gmail.com>
2025-03-03 18:24:43 +0200

    New query class to handle query building

--- src/literator/api/base.py
+++ src/literator/api/base.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import logging
-from typing import List, Dict, Any, Literal, Optional
+from typing import List, Dict, Any, Literal, Optional, Self, Union
 
 from literator.config import get_api_config
 from literator.db.models import Paper
@@ -84,6 +84,91 @@ class APIClient(ABC):
     @abstractmethod
     def parse_results(self, entries: List[Dict[str, Any]]) -> List[Paper]:
         pass
+
+
+SQUOTE = "'"
+DQUOTE = '"'
+SPACE = " "
+
+
+class APIQuery:
+
+    def __init__(self):
+        self.terms: List[str] = []
+        self.constraints: List[str] = {}
+        self.inclusions: List[str] = []
+        self.exclusion: List[str] = []
+        self.start_year: Optional[int] = None
+        self.end_year: Optional[int] = None
+
+    def wrap(self, term: str) -> str:
+        # Check if word is wrapped in quotes
+        if (term.startswith(DQUOTE) and term.endswith(DQUOTE)) or (
+            term.startswith(SQUOTE) and term.endswith(DQUOTE)
+        ):
+            return term
+        else:
+            return f'"{term}"'
+
+    def add_term(self, term: str, exact: bool = False) -> Self:
+        term = term.strip()
+
+        # Multiple words or
+        if SPACE in term or exact:
+            self.terms.append(self.wrap(term))
+        else:
+            self.terms.append(term)
+
+        return self
+
+    def add_terms(
+        self, terms: List[str], exact: Union[bool, List[bool]] = False
+    ) -> Self:
+        if isinstance(exact, bool):
+            exact = [exact] * len(terms)
+
+        for term, ex in zip(terms, exact):
+            self.add_term(term, ex)
+
+        return self
+
+    def include_term(self, term: str, exact: bool = False) -> Self:
+        term = term.strip()
+        wrapped_term = self.wrap(term)
+        is_term = term in self.terms
+        is_exact_term = wrapped_term in self.terms
+        is_inclusion = term in self.inclusions
+        is_exact_inclusion = wrapped_term in self.inclusions
+        is_exclusion = term in self.exclusion
+        is_exact_exclusion = wrapped_term in self.exclusion
+
+        # Is a term, moved to inclusions as exact
+        if exact and is_term:
+            self.terms.remove(term)
+            self.inclusions.append(wrapped_term)
+
+        # Is an exact term, moved to inclusions as exact
+        elif exact and is_exact_term:
+            self.terms.remove(wrapped_term)
+            self.inclusions.append(term)
+
+        # Is an term, moved to inclusions as non-exact
+        elif exact and is_exact_term:
+            self.terms.remove(wrapped_term)
+            self.inclusions.append(term)
+
+        #
+        elif not exact and is_term:
+            self.terms.remove(term)
+            self.inclusions.append(term)
+        elif not exact and is_exact_term:
+            self.terms.remove(wrapped_term)
+            self.inclusions.append(wrapped_term)
+        elif not exact and is_inclusion:
+            self.inclusions.remove(term)
+            self.terms.append(term)
+        elif not exact and is_exact_inclusion:
+            self.inclusions.remove(wrapped_term)
 
 
 def get_api_client(name: Literal["scopus"]) -> APIClient:
