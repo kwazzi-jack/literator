bb6e13251749f00a31a4e198758419bcbb96a448
Brian Welman <brianallisterwelman@gmail.com>
2025-03-05 12:47:26 +0200

    New function to get datetime from string timestamp

--- literator/utils/__init__.py
+++ literator/utils/__init__.py
@@ -1,7 +1,7 @@
 """Utility functions for the Literator package."""
 
 # Expose module
-from literator.utils.functions import get_timestamp
+from literator.utils.functions import get_timestamp, str_to_datetime
 from literator.utils.logging import setup_logging, get_logger
 
-__all__ = ["get_timestamp", "setup_logging", "get_logger"]
+__all__ = ["get_timestamp", "str_to_datetime", "setup_logging", "get_logger"]
--- literator/utils/functions.py
+++ literator/utils/functions.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timezone
 
 
-def get_timestamp() -> str:
+def get_timestamp(pattern: str = "%Y%m%d_%H%M%S") -> str:
     """Get the current timestamp in a format suitable for filenames."""
-    return datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
+    return datetime.now(timezone.utc).strftime(pattern)
+
+
+def str_to_datetime(timestamp: str, pattern: str = "%Y%m%d_%H%M%S") -> datetime:
+    return datetime.strptime(timestamp, pattern)
