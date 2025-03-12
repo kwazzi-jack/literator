84159102e3eebb921701c29c2c6aa8b3b6c6a8ff
Brian Welman <brianallisterwelman@gmail.com>
2025-03-12 11:25:15 +0200

    Rename error types for clarity and enhance type exposure in utils module

--- literator/utils/__init__.py
+++ literator/utils/__init__.py
@@ -3,14 +3,14 @@
 # Expose module
 from literator.utils.errors import (
     Error,
-    ValidationErr,
-    PhraseErr,
+    ValidationError,
+    PhraseError,
     PhraseEmptyError,
     PhrasesEmptyError,
-    PhraseTypeErr,
-    PhrasesTypeErr,
-    FlagErr,
-    FlagsEmptyErr,
+    PhraseTypeError,
+    PhrasesTypeError,
+    FlagError,
+    FlagsEmptyError,
     FlagTypeError,
 )
 from literator.utils.functions import get_timestamp, str_to_datetime, format_phrases
@@ -18,34 +18,43 @@ from literator.utils.logging import setup_logging, get_logger
 from literator.utils.types import (
     Date,
     Flag,
+    FlagInput,
     FlagResult,
     Flags,
     Phrase,
+    PhraseInput,
     PhraseResult,
     Phrases,
+    ValidationResult,
 )
 
 __all__ = [
+    # Errors
     "Error",
-    "ValidationErr",
-    "PhraseErr",
+    "ValidationError",
+    "PhraseError",
     "PhraseEmptyError",
     "PhrasesEmptyError",
-    "PhraseTypeErr",
-    "PhrasesTypeErr",
-    "FlagErr",
-    "FlagsEmptyErr",
+    "PhraseTypeError",
+    "PhrasesTypeError",
+    "FlagError",
+    "FlagsEmptyError",
     "FlagTypeError",
+    # Functions
     "get_timestamp",
     "str_to_datetime",
     "format_phrases",
     "setup_logging",
     "get_logger",
+    # Types
     "Date",
     "Flag",
+    "FlagInput",
     "FlagResult",
     "Flags",
     "Phrase",
+    "PhraseInput",
     "PhraseResult",
     "Phrases",
+    "ValidationResult",
 ]
