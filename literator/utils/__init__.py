241e4324d8e426197eb1b61b1bb4e8521cd3b75d
Brian Welman <brianallisterwelman@gmail.com>
2025-03-12 09:43:20 +0200

    Improved typing scheme to handle input and output types

--- literator/utils/__init__.py
+++ literator/utils/__init__.py
@@ -1,18 +1,19 @@
 """Utility functions for the Literator package."""
 
 # Expose module
-from literator.utils.functions import get_timestamp, str_to_datetime, format_phrase
+from literator.utils.functions import get_timestamp, str_to_datetime, format_phrases
 from literator.utils.logging import setup_logging, get_logger
-from literator.utils.types import Date, Phrases, PhraseInput, ExactFlag
+from literator.utils.types import BoolInput, BoolOutput, Date, StrInput, StrOutput
 
 __all__ = [
     "get_timestamp",
     "str_to_datetime",
-    "format_phrase",
+    "format_phrases",
     "setup_logging",
     "get_logger",
+    "BoolInput",
+    "BoolOutput",
     "Date",
-    "Phrases",
-    "PhraseInput",
-    "ExactFlag",
+    "StrInput",
+    "StrOutput",
 ]
--- literator/utils/types.py
+++ literator/utils/types.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from typing import List, Optional, Sequence, TypeAlias, Union
+from typing import Optional, Sequence, TypeAlias, Union
 
+# Input types
+StrInput: TypeAlias = Union[str, Sequence[str]]
+BoolInput: TypeAlias = Union[bool, Sequence[bool]]
 
+# Output types
+StrOutput: TypeAlias = Union[str, Sequence[str]]
+BoolOutput: TypeAlias = Union[bool, Sequence[bool]]
+
+# Other types
 Date: TypeAlias = Optional[datetime]
-Phrases: TypeAlias = List[str]
-PhraseInput: TypeAlias = Union[str, Sequence[str]]
-ExactFlag: TypeAlias = Union[bool, Sequence[bool]]
