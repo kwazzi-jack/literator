698904260446b59e9aeb88aef2684563c7c24468
Brian Welman <brianallisterwelman@gmail.com>
2025-03-12 09:29:46 +0200

    Expose format_phrase function and add utility constants for string formatting

--- literator/utils/__init__.py
+++ literator/utils/__init__.py
@@ -1,7 +1,13 @@
 """Utility functions for the Literator package."""
 
 # Expose module
-from literator.utils.functions import get_timestamp, str_to_datetime
+from literator.utils.functions import get_timestamp, str_to_datetime, format_phrase
 from literator.utils.logging import setup_logging, get_logger
 
-__all__ = ["get_timestamp", "str_to_datetime", "setup_logging", "get_logger"]
+__all__ = [
+    "get_timestamp",
+    "str_to_datetime",
+    "format_phrase",
+    "setup_logging",
+    "get_logger",
+]
--- literator/utils/functions.py
+++ literator/utils/functions.py
@@ -1,5 +1,10 @@
 from datetime import datetime, timezone
 
+SQUOTE = "'"
+DQUOTE = '"'
+SPACE = " "
+COMMA = ","
+
 
 def get_timestamp(pattern: str = "%Y%m%d_%H%M%S") -> str:
     """Get the current timestamp in a format suitable for filenames."""
@@ -8,3 +13,14 @@ def get_timestamp(pattern: str = "%Y%m%d_%H%M%S") -> str:
 
 def str_to_datetime(timestamp: str, pattern: str = "%Y%m%d_%H%M%S") -> datetime:
     return datetime.strptime(timestamp, pattern)
+
+
+def format_phrase(value: str, exact: bool = False) -> str:
+    strip_val = value.strip()
+    wrapped = (strip_val.startswith(SQUOTE) and strip_val.endswith(SQUOTE)) or (
+        strip_val.startswith(DQUOTE) and strip_val.endswith(DQUOTE)
+    )
+    if not wrapped and (exact or SPACE in value or COMMA in value):
+        return f'"{strip_val}"'
+    else:
+        return strip_val
